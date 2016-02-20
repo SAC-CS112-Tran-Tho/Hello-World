1141349c251ceb47f8030c48972bff782e170546
SAC-CS112-Tran-Tho <tho.van.tran@gmail.com>
2016-02-20 00:30:29 -0800

    Added files via upload

---
+++ HelloSAC.java
@@ -0,0 +1,18 @@
+/*
+ * HW#2
+ * Outputting Text
+ */
+package hellosac;
+
+/**
+ * @author VTh0
+ */
+public class HelloSAC 
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello, Santa Ana");
+   }
+   
+}
---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * HW #1
+ * Outputting text
+ */
+package helloworld;
+
+/**
+ * @author VTh0
+ */
+public class HelloWorld 
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello World!");
+   }
+   
+}
