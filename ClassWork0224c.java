216712ed9cb9e5a0a525b388523db7b055e11856
SAC-CS112-Tran-Tho <tho.van.tran@gmail.com>
2016-02-24 20:50:42 -0800

    Added files via upload

---
+++ ClassWork0224c.java
@@ -0,0 +1,14 @@
+
+package classwork0224c;
+
+// Tho Tran
+
+public class ClassWork0224c 
+{
+   public static void main(String[] args) 
+   {
+      String greeting = "Hello, World";
+      System.out.println(greeting);
+   }
+   
+}
