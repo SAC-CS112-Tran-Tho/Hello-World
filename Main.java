002b299aff1b4c3fce7fc33eb4730f639b73941b
SAC-CS112-Tran-Tho <tho.van.tran@gmail.com>
2016-02-24 21:31:15 -0800

    Added files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+// Tho Tran
+
+package pkg0224e;
+import java.util.Scanner;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      
+      int num1;
+      int num2;
+      
+      System.out.print("Enter first integer: ");
+      num1 = input.nextInt();
+      
+      System.out.print("Enter second integer: ");
+      num2 = input.nextInt();
+      
+      System.out.printf("Hello, you entered " + num1 + " and " + num2);
+   }
+   
+}
