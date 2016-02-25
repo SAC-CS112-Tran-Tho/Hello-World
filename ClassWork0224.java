1431f5a99f6123f5a154077e2576cee9e487e3a1
SAC-CS112-Tran-Tho <tho.van.tran@gmail.com>
2016-02-24 20:03:07 -0800

    Added files via upload

---
+++ ClassWork0224.java
@@ -0,0 +1,13 @@
+// Classwork 02/24
+package classwork02.pkg24;
+
+// Tho Tran
+public class ClassWork0224 
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello");
+      System.out.println("My name is Tho Tran");
+   }
+   
+}
