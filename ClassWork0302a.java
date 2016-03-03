afb4c9e3d870b8fac4deb02faef26543c9abf2d8
SAC-CS112-Tran-Tho <tho.van.tran@gmail.com>
2016-03-02 20:03:30 -0800

    Added files via upload

---
+++ ClassWork0302a.java
@@ -0,0 +1,12 @@
+// Classwork03-02
+package classwork03.pkg02a;
+// Tho Tran
+public class ClassWork0302a 
+{
+   public static void main(String[] args) 
+   {
+     System.out.printf("%s%n%s%n%s%n%s%n%s%n%s%n", "Hungry?", "Get in line", 
+             "Buy lunch", "Eat lunch", "Return tray", "Leave");
+   }
+   
+}
