25a11532480e5b0ea706a88c11d3ab380808c709
Tho Tran <tho.van.tran@gmail.com>
2016-03-16 20:07:12 -0700

    Added files via upload

---
+++ ClassWork0316c.java
@@ -0,0 +1,34 @@
+// Tho Tran
+// Classwork 03-16c
+// This program implements a guess-a-number game with else-if and else statements
+package classwork03.pkg16c;
+
+import java.util.Scanner;
+
+public class ClassWork0316c 
+{
+   public static void main(String[] args) 
+   {
+      Scanner input = new Scanner (System.in);
+      
+      int number;
+      int computerNum;
+    
+      computerNum = 1 +(int)(Math.random()*10);
+      
+      System.out.print("Enter a number (1-10): ");
+      number = input.nextInt();
+      
+      System.out.printf("The random number is: " + computerNum + "\n");
+      
+      if (number > computerNum)
+         System.out.print("The number you entered is too big!\n");
+      else if (number < computerNum)
+         System.out.print("The number you entered is too small!\n");
+      else if (number == computerNum)
+         System.out.print("The number you entered is correct!\n");
+      else
+         System.out.print("The number you entered was not within range!\n");
+   }
+   
+}
