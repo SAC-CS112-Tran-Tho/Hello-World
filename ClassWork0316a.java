aa9f8243d48c74d3e51d9556cce0e31a56ada99a
Tho Tran <tho.van.tran@gmail.com>
2016-03-16 19:39:37 -0700

    Added files via upload

---
+++ ClassWork0316a.java
@@ -0,0 +1,31 @@
+// Tho Tran
+// Classwork 03-16a
+// This program implements a guess-a-number game
+package classwork03.pkg16a;
+
+import java.util.Scanner;
+
+public class ClassWork0316a
+{
+   public static void main(String[] args) 
+   {
+      Scanner input = new Scanner (System.in);
+      
+      int computerNum;
+      int number;
+      
+      computerNum = 1 +(int)(Math.random()*10);
+      
+      System.out.print("Enter a number: ");
+      number = input.nextInt();
+      
+      System.out.printf("The random number is: " + computerNum + "\n");
+      
+      if (number > computerNum)
+           System.out.print("The number you entered is too big!\n");
+      if (number < computerNum)
+           System.out.print("The number you entered is too small!\n");
+      if (number == computerNum)
+           System.out.print("The number you entered is correct!\n");
+   }
+}
