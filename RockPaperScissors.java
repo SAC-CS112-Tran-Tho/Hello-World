c1b83adefa167b58b7bf49a5d2b9ab93314c79de
Tho Tran <tho.van.tran@gmail.com>
2016-03-23 20:53:41 -0700

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -12,45 +12,47 @@ public class RockPaperScissors
    {
      Scanner input = new Scanner ( System.in );
      
-     String choice1;    // user enters rock, paper, or scissor
-     String choice2;    // user enters another of the three choices
+     String userChoice;    // user enters rock, paper, or scissor
+     int computerChoice;
+     String computerPlay;
      int counter = 1;
      
      while (counter < 4)
      {
-     System.out.println("Let's play Rock, Paper, Scissor!\n");
+     computerChoice = 1 +(int)(Math.random()*3);
+        
+     System.out.println("Let's play (R)ock, (P)aper, (S)cissor!\n");
      System.out.print("Enter your favorite pick: ");
-     choice1 = input.nextLine();       // user enters first pick
+     userChoice = input.nextLine();       // user enters first pick
      
-     System.out.print("Enter your 2nd favorite pick: ");
-     choice2 = input.nextLine();       // user enters second pick
-     
-     if ( choice1.equals(choice2))     // tie game if the choices are the same
-        System.out.println("Tie game!!\n");
-     else if (choice1.equals("Rock") || choice1.equals("rock"))   // if rock
+     if (computerChoice == 1)
      {
-        if (choice2.equals("Scissor") || choice2.equals("scissor"))
-           System.out.println("Rock beats scissors. You win!!\n");  // rock wins
-        else if (choice2.equals("Paper") || choice2.equals("paper"))
-           System.out.println("Paper beats rock. You lose!\n");     // rock loses
+        computerPlay = "R";
      }
-     else if (choice1.equals("Paper") || choice1.equals("paper")) // if paper
+     else if (computerChoice == 2)
      {
-        if (choice2.equals("Scissor") || choice2.equals("scissor"))
-           System.out.println("Scissor cuts paper. You lose!\n");   // paper loses
-        else if (choice2.equals("Rock") || choice2.equals("rock"))
-           System.out.println("Paper beats rock. You win!\n");      // paper wins
-     }
-     else if (choice1.equals("Scissor") || choice1.equals("scissor")) // if scissor
-     {
-        if (choice2.equals("Paper") || choice2.equals("paper"))
-           System.out.println("Scissor cuts paper. You win!\n");    // scissor wins
-        else if (choice2.equals("Rock") || choice2.equals("rock"))
-           System.out.println("Rock beats scissor. You lose!\n");   // scissor loses
+        computerPlay = "P";
      }
      else
      {
-           System.out.println("Invalid input. Try again.\n");  // choice does not match
+        computerPlay = "S";
+     }
+     
+     System.out.printf("The computer picked: " + computerPlay + "\n");
+     
+     if (userChoice.equals(computerPlay))     // tie game if the choices are the same
+     {
+        System.out.println("Tie game!!\n");
+     }
+     else if ((userChoice.equals("R") && computerPlay.equals("P")) ||
+             ((userChoice.equals("P") && computerPlay.equals("S")))||
+             ((userChoice.equals("S") && computerPlay.equals("R"))))
+     {
+        System.out.println("You lose!\n");      
+     }
+     else
+     {
+           System.out.println("You win!\n");
      }
      
      counter++;
