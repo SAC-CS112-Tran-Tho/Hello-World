e7c1880d78ccf996ca00858682882a0ae78bcaa5
Tho Tran <tho.van.tran@gmail.com>
2016-03-30 19:43:06 -0700

    Added files via upload

--- ClassWork0330a.java
+++ ClassWork0330a.java
@@ -11,21 +11,27 @@ public class ClassWork0330a
    {
       Scanner input = new Scanner (System.in);
       
+      Calendar thisYear = Calendar.getInstance();
       int birthYear;
       int userAge;
-      String name;
-      
-      Calendar thisYear = Calendar.getInstance();
-      
+      int legalAge = 21;
+      String name;     
+            
       System.out.print("Enter your name: ");
       name = input.nextLine();
       
       System.out.print("Enter your birth year: ");
       birthYear = input.nextInt();
       
-      userAge = thisYear.get(Calendar.YEAR) - birthYear;
+      userAge = thisYear.get(Calendar.YEAR) - birthYear;      
       
       System.out.printf("Hello, " + name + ", you're " + userAge + " years-old today!\n");
+            
+      if (userAge >= legalAge)
+         System.out.print("You are old enough to drink!\n");
+      else
+         System.out.printf("Sorry, you are not old enough to drink. You must wait " + (legalAge - userAge) + " more years to drink!\n");
+         
    }
    
 }
