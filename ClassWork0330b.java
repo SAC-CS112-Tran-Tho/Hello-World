b5c3b444ba764eb0551486ae3cd8fc973f22a69c
Tho Tran <tho.van.tran@gmail.com>
2016-03-30 19:56:57 -0700

    Added files via upload

---
+++ ClassWork0330b.java
@@ -0,0 +1,36 @@
+// Tho Tran
+// Classwork 03-30b
+package classwork03.pkg30b;
+
+import java.util.Scanner;
+import java.util.Calendar;
+
+public class ClassWork0330b 
+{
+   public static void main(String[] args) 
+   {
+    Scanner input = new Scanner (System.in);
+      
+    Calendar thisYear = Calendar.getInstance();
+    int birthYear;
+    int userAge;
+    int legalAge = 21;
+    String name;     
+            
+    System.out.print("Enter your name: ");
+    name = input.nextLine();
+      
+    System.out.print("Enter your birth year: ");
+    birthYear = input.nextInt();
+      
+    userAge = thisYear.get(Calendar.YEAR) - birthYear;      
+      
+    System.out.printf("Hello, " + name + ", you're " + userAge + " years-old today!\n");
+            
+    if (userAge >= legalAge)
+      System.out.print("You are old enough to drink!\n");
+    else
+      System.out.printf("Sorry, you are not old enough to drink. You must wait " + (legalAge - userAge) + 
+               " more years to drink until " + (birthYear + 21) + "!\n");         
+   }
+}
