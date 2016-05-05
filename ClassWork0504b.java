1bc36127c21e26ac7a21d62e70950cf4c230d919
Tho Tran <tho.van.tran@gmail.com>
2016-05-04 22:33:47 -0700

    Create ClassWork0504b.java

---
+++ ClassWork0504b.java
@@ -0,0 +1,53 @@
+
+package classwork05.pkg03b;
+
+public class ThreeDice
+{
+   private DiceW12 d1,d2,d3;
+
+   public int Throw()
+   {
+      int randomNum;
+      
+      randomNum = (d1.Throw()+d2.Throw()+d3.Throw())/3;
+
+      return randomNum;
+   }
+}
+
+package classwork05.pkg03b;
+
+public class DiceW12
+{
+   private static int randomNumber;
+   
+   public static int Throw()
+   {
+      randomNumber = ((int)(Math.random()*6)+1);
+      return randomNumber;
+   }
+   public static int Throw(int dices)
+   {
+      randomNumber = dices * ((int)(Math.random()*6)+1);
+      return randomNumber;
+   }
+   public static int Throw(int dices, int bounces)
+   {
+      randomNumber = dices *((int)(Math.random()*6)+1)+bounces;
+      return randomNumber;
+   }
+}
+
+package classwork05.pkg03b;
+
+public class ClassWork0503b
+{
+   public static void main(String[] args)
+   {
+      ThreeDice dice = new ThreeDice();
+      
+      System.out.println("3 dices are rolled and the average number is " + dice.Throw());
+
+   }
+   
+}
