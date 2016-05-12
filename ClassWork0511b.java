19e3d9334e143448850547723e7483ef5da418c6
Tho Tran <tho.van.tran@gmail.com>
2016-05-11 20:50:14 -0700

    Create ClassWork0511b.java

---
+++ ClassWork0511b.java
@@ -0,0 +1,51 @@
+
+package classwork05.pkg11a;
+
+public class TripleDice extends Dice
+{
+   private int randomNum;
+   
+   @Override
+   public int Throw()
+   {
+      randomNum=(super.Throw()+super.Throw()+super.Throw())/3;
+      return randomNum;
+   }
+}
+
+
+package classwork05.pkg11a;
+
+public class Dice
+{
+   private int randomNum;
+   
+   public int Throw()
+   {
+      randomNum = ((int)(Math.random()*6)+1);
+      return randomNum;
+   }
+   public int Throw(int dices)
+   {
+      randomNum = dices * ((int)(Math.random()*6)+1);
+      return randomNum;
+   }
+   public int Throw(int dices, int bounces)
+   {
+      randomNum = dices *((int)(Math.random()*6)+1)+bounces;
+      return randomNum;
+   }
+}
+
+
+package classwork05.pkg11a;
+
+public class ClassWork0511a
+{
+   public static void main(String[] args)
+   {
+      TripleDice dice = new TripleDice();
+      
+      System.out.printf("We rolled 3 dice and the average is: %d\n", dice.Throw());
+   }
+}
