a9b75ed27667a1a0f9e2667b4d306d3b130b701f
Tho Tran <tho.van.tran@gmail.com>
2016-05-25 16:28:26 -0700

    Delete RandomNumber.java

--- RandomNumber.java
+++
@@ -1,9 +0,0 @@
-
-package classwork05.pkg18a;
-
-public interface RandomNumber
-{
-   int getRandomNumber();     // returns a random number; no implementation
-   int getHistory();          // returns number of tries; no implementation
-   void Throw();              // generates number of tries for history and random number
-}  // end interface RandomNumber
